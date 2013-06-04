eecaea18cb7561c297e64b88acba159bcebe8b4c
Abhinav Rathod <ar61@localhost.(none)>
2013-06-04 01:02:25 -0700

    Initial Commit

---
+++ GravityTwist/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  GravityTwist
+//
+//  Created by Abhinav Rathod on 6/4/13.
+//  Copyright 526 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
