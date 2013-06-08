4ceb395c390cee28ff56c9fb8c9453cb4ee52edf
Abhinav <rathod.abhinav@gmail.com>
2013-06-07 21:29:44 -0700

    fixed errors

--- GravityTwist/HelloWorldLayer.mm
+++ GravityTwist/HelloWorldLayer.mm
@@ -27,6 +27,7 @@ enum {
 @interface HelloWorldLayer()
 {
     CCPhysicsSprite *player;
+    b2Body *body;
     CGSize s;
 }
 -(void) initPhysics;
@@ -56,6 +57,8 @@ enum {
 	if( (self=[super init])) {
 		
 		// enable events
+        
+        tiledMap = [CCTMXTiledMap tiledMapWithTMXFile:@"lvl1.tmx"];
 		
         tile = [tiledMap layerNamed:@"tiles"];
         //meta = [tiledMap layerNamed:@"meta"];
@@ -99,7 +102,7 @@ enum {
         b2BodyDef bodyDef;
         bodyDef.type = b2_dynamicBody;
         bodyDef.position.Set(s.width/PTM_RATIO, s.height/PTM_RATIO);
-        b2Body *body = world->CreateBody(&bodyDef);
+        body = world->CreateBody(&bodyDef);
         
         // Define another box shape for our dynamic body.
         b2PolygonShape dynamicBox;
@@ -124,7 +127,7 @@ enum {
         
         [player setPTMRatio:PTM_RATIO];
         [player setB2Body:body];
-        [player setPosition: ccp( s.width/2, s.height/2)];
+        [player setPosition: ccp(x,y)];
         
         
 		//adding buttons
@@ -137,7 +140,9 @@ enum {
         CCMenu *menu2 = [CCMenu menuWithItems:item2,nil];
         menu2.position = ccp(120,40);
         [self addChild:menu2];
-        		
+        
+		
+        [self addChild:tiledMap z:-1];
 		[self scheduleUpdate];
 	}
 	return self;
@@ -145,18 +150,25 @@ enum {
 
 -(void)moveLeft
 {
+    /*
     if((player.position.x - 50) > 0)
     {
         [player runAction:[CCMoveBy actionWithDuration:.3 position:ccp(-50,0)]];
     }
+    */
+    
+    body->ApplyForceToCenter(b2Vec2(-80,0));
 }
 
 -(void)moveRight
 {
+    /*
     if((player.position.x + 50) < s.width)
     {
         [player runAction:[CCMoveBy actionWithDuration:.3 position:ccp(50,0)]];
     }
+    */
+    body->ApplyForceToCenter(b2Vec2(80,0));
 }
 
 
@@ -174,7 +186,7 @@ enum {
 -(void) initPhysics
 {
 	
-	CGSize size = [[CCDirector sharedDirector] winSize];
+	CGSize s = [[CCDirector sharedDirector] winSize];
 	
 	b2Vec2 gravity;
 	gravity.Set(0.0f, -10.0f);
@@ -212,19 +224,19 @@ enum {
 	
 	// bottom
 	
-	groundBox.Set(b2Vec2(0,0), b2Vec2(size.width/PTM_RATIO,0));
+	groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/PTM_RATIO,0));
 	groundBody->CreateFixture(&groundBox,0);
 	
 	// top
-	groundBox.Set(b2Vec2(0,size.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,size.height/PTM_RATIO));
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO));
 	groundBody->CreateFixture(&groundBox,0);
 	
 	// left
-	groundBox.Set(b2Vec2(0,size.height/PTM_RATIO), b2Vec2(0,0));
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(0,0));
 	groundBody->CreateFixture(&groundBox,0);
 	
 	// right
-	groundBox.Set(b2Vec2(size.width/PTM_RATIO,size.height/PTM_RATIO), b2Vec2(size.width/PTM_RATIO,0));
+	groundBox.Set(b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,0));
 	groundBody->CreateFixture(&groundBox,0);
 }
 
@@ -245,6 +257,43 @@ enum {
 	
 	kmGLPopMatrix();
 }
+
+/*-(void) addNewSpriteAtPosition:(CGPoint)p
+{
+	CCLOG(@"Add sprite %0.2f x %02.f",p.x,p.y);
+	// Define the dynamic body.
+	//Set up a 1m squared box in the physics world
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
+	b2Body *body = world->CreateBody(&bodyDef);
+	
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
+	
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;	
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+	body->CreateFixture(&fixtureDef);
+	
+
+	CCNode *parent = [self getChildByTag:kTagParentNode];
+	
+	//We have a 64x64 sprite sheet with 4 different 32x32 images.  The following code is
+	//just randomly picking one of the images
+	int idx = (CCRANDOM_0_1() > .5 ? 0:1);
+	int idy = (CCRANDOM_0_1() > .5 ? 0:1);
+	CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(32 * idx,32 * idy,32,32)];
+	[parent addChild:sprite];
+	
+	[sprite setPTMRatio:PTM_RATIO];
+	[sprite setB2Body:body];
+	[sprite setPosition: ccp( p.x, p.y)];
+
+}*/
 
 -(void) update: (ccTime) dt
 {
@@ -331,9 +380,43 @@ enum {
 
 -(void) ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    [player runAction:[CCJumpTo actionWithDuration:1.0f position:player.position height:50 jumps:1]];
+    //[player runAction:[CCJumpTo actionWithDuration:1.0f position:player.position height:50 jumps:1]];
+    // check if the player is not moving on the y axis already
+    CGFloat yvel = body->GetLinearVelocity().y;
+    if (yvel == 0.0f) {
+        body->ApplyLinearImpulse(b2Vec2(0, 10), body->GetWorldCenter());
+    } else {
+        printf("%f\n",yvel);
+    }
 }
 
+/*- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	//Add a new body/atlas sprite at the touched location
+	for( UITouch *touch in touches ) {
+		CGPoint location = [touch locationInView: [touch view]];
+		
+		location = [[CCDirector sharedDirector] convertToGL: location];
+		
+        //move character
+        
+        
+		//[self addNewSpriteAtPosition: location];
+	}
+}*/
 
+#pragma mark GameKit delegate
+
+-(void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
+
+-(void) leaderboardViewControllerDidFinish:(GKLeaderboardViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
 
 @end
