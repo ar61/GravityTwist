a8035bd09d4efe3ed56007fe5ed75ee912f0f65f
Abhinav <rathod.abhinav@gmail.com>
2013-06-10 10:16:04 -0700

    Changes for collision detection

---
+++ GravityTwist/MyContactListener.h
@@ -0,0 +1,13 @@
+//
+//  b2ContactListener+ContactListener.h
+//  GravityTwist
+//
+//  Created by Abhinav Rathod on 6/9/13.
+//  Copyright (c) 2013 526. All rights reserved.
+//
+
+
+
+@interface b2ContactListener (ContactListener)
+
+@end
---
+++ GravityTwist/MyContactListener.mm
@@ -0,0 +1,13 @@
+//
+//  b2ContactListener+ContactListener.m
+//  GravityTwist
+//
+//  Created by Abhinav Rathod on 6/9/13.
+//  Copyright (c) 2013 526. All rights reserved.
+//
+
+#import "b2ContactListener+ContactListener.h"
+
+@implementation b2ContactListener (ContactListener)
+
+@end
